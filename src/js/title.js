6a146bba0cf62531e70887b47c5eb050434607d8
Bart Koelewijn <bart.koelewijn@wearejust.com>
2021-11-03 22:20:30 +0100

    Feature: title anim

--- src/js/title.js
+++ src/js/title.js
@@ -13,7 +13,13 @@ export function init() {
 class Title {
     constructor(element) {
         this.element = element;
+        setInterval(()=> {
+            this.animateTitles();
+        }, 8000)
+        this.animateTitles();
+    }
 
+    animateTitles() {
         const tl = gsap.timeline({
             defaults: {
                 duration: 0.5,
@@ -22,7 +28,17 @@ class Title {
         tl.to(this.element.find('.js-title-part-1'), {
             transform: 'translateY(0%)',
         }, 'key-1')
-        
-
+        tl.to(this.element.find('.js-title-part-1'), {
+            transform: 'translateY(100%)',
+            delay: 3,
+        }, 'key-1')
+        tl.to(this.element.find('.js-title-part-2'), {
+            transform: 'translateY(0%)',
+            delay: 4,
+        }, 'key-1')
+        tl.to(this.element.find('.js-title-part-2'), {
+            transform: 'translateY(100%)',
+            delay: 7,
+        }, 'key-1')
     }
 }
