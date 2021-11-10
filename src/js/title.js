ab925cfc6ea560972ded66110374e177a9cebc71
Bart Koelewijn <bart.koelewijn@wearejust.com>
2021-11-10 22:59:55 +0100

    Feature: mock shifting

--- src/js/title.js
+++ src/js/title.js
@@ -28,16 +28,37 @@ class Title {
         tl.to(this.element.find('.js-title-part-1'), {
             transform: 'translateY(0%)',
         }, 'key-1')
+        tl.to('.js-mock-mobile', {
+            transform: 'translateX(0%)',
+            opacity: 1,
+        }, 'key-1')
         tl.to(this.element.find('.js-title-part-1'), {
             transform: 'translateY(100%)',
             delay: 3,
+        }, 'key-1')
+        tl.to('.js-mock-mobile', {
+            transform: 'translateX(20%)',
+            opacity: 0,
+            delay: 3,
+            duration: 0.6,
         }, 'key-1')
         tl.to(this.element.find('.js-title-part-2'), {
             transform: 'translateY(0%)',
             delay: 4,
         }, 'key-1')
+        tl.to('.js-mock-desktop', {
+            transform: 'translateX(0%)',
+            opacity: 1,
+            delay: 4,
+            duration: 0.6,
+        }, 'key-1')
         tl.to(this.element.find('.js-title-part-2'), {
             transform: 'translateY(100%)',
+            delay: 7,
+        }, 'key-1')
+        tl.to('.js-mock-desktop', {
+            transform: 'translateX(20%)',
+            opacity: 0,
             delay: 7,
         }, 'key-1')
     }
